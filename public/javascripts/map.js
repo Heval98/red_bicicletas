398de1822a266bfa60c6320dfc7772712c5e4c1f
Heval98 <jmheval@gmail.com>
2020-08-23 23:29:34 -0500

    api de bicicletas

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,5 +1,20 @@
-var mymap = L.map('main_map').setView([51.505, -0.09], 13);
+var mymap = L.map('main_map').setView([-34.6012424, -58.3861497], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap); 
+
+// L.marker([-34.6012424, -58.3861497]).addTo(mymap);
+// L.marker([-34.9387450, -58.4562837]).addTo(mymap);
+
+
+$.ajax({
+    dataType : "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici) {
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(mymap);
+        });
+    }
+});
