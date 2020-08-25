42a7b868f3e4d2e0540fcda29270a1c4c2508d64
Heval98 <jmheval@gmail.com>
2020-08-25 12:00:42 -0500

    JSON web token

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,61 @@
 var mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 var Reserva = require('./reserva');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 var Schema = mongoose.Schema;
 
+var Schema = mongoose.Schema;
+
+const validateEmail = function(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
 var usuarioSchema = new Schema({
-    nombre: String,
+    nombre: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        unique: true,
+        validate: [validateEmail, 'Por favor ingrese email valido'],
+        match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/]
+    },
+    password: {
+        type: String,
+        required: [true, 'Password es obligatorio']
+    },
+    //Datos para verificacion por email
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado: {
+        type: Boolean,
+        default: false
+    }
 });
+
+usuarioSchema.plugin(uniqueValidator, {message: 'El {PATH} ya existe con otro usuario'})
+
+//Previo al save se ejecuta la funcion
+usuarioSchema.pre('save', function(next){
+    if(this.isModified('password')){
+        //Encriptamos la contraseña
+        this.password = bcrypt.hashSync(this.password, saltRounds)
+    }
+    next();
+});
+
+usuarioSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
+
 
 usuarioSchema.methods.reservar = function(biciId, desde, hasta, cb){
     var reserva = new Reserva({usuario: this._id, bicicleta: biciId, desde: desde, hasta: hasta});
